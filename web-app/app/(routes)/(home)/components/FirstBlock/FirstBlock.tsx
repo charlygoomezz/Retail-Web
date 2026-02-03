28beaea135ba986a1c6578bdae54773fa5b0dbf2
Carlos Gómez <carlosgomzp21@gmail.com>
2026-02-03 13:41:20 +0100

    Create brands carousel

--- web-app/app/(routes)/(home)/components/FirstBlock/FirstBlock.tsx
+++ web-app/app/(routes)/(home)/components/FirstBlock/FirstBlock.tsx
@@ -1,15 +1,14 @@
 'use client';
 import { Reveal } from '@/components/Shared/Reveal';
 import Image from 'next/image';
-import React from 'react';
 
 export function FirstBlock() {
   return (
     <div className="grid lg:grid-cols-2 lg:px-0 lg:py-24 items-center">
       <Reveal className="p-6 lg:pl-40" position="bottom">
-        <h1 className="text-6xl lg:text-8xl font-bold">
+        <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold">
           Premium
-          <span className="block">Car Rental </span> in Spain
+          <span className="block">Rental Cars </span> in Spain
         </h1>
         <p className="text-lg mt-2 lg:mt-5 lg:text-lg max-w-sm">
           Dont deny yourself pleasure of diving the best premium cars from around the world here and now
---
+++ web-app/app/(routes)/(home)/components/SliderBrands/SliderBrands.data.ts
@@ -0,0 +1,35 @@
+export const dataBrands = [
+  {
+    url: 'AstonMartinLogo.png',
+  },
+  {
+    url: 'Audi-Logo_2016.png',
+  },
+  {
+    url: 'BMW.png',
+  },
+  {
+    url: 'Ferrari-Logo.png',
+  },
+  {
+    url: 'Group (1).png',
+  },
+  {
+    url: 'Group.png',
+  },
+  {
+    url: 'Maserati_logo.png',
+  },
+  {
+    url: 'McLaren_logo 1.png',
+  },
+  {
+    url: 'Mercedes-Benz_free_logo.png',
+  },
+  {
+    url: 'Rolls-Royce_Motor_Cars_logo.png',
+  },
+  {
+    url: 'lamborghini 1.png',
+  },
+];
